39f5a51e235b0f37532f982263c4763bf835c438
Dario Schor <dario@schor.ca>
2015-11-14 11:38:12 -0600

    Updated header comments for lab 4.

--- solutions/lab4.ino
+++ solutions/lab4.ino
@@ -1,3 +1,35 @@
+/***
+ * Millenium Library - Arduino Workshop
+ * 
+ * TITLE:        Lab #4
+ * DESCRIPTION:  Servo Controls
+ * AUTHOR:       Dario Schor (schor@ieee.org)
+ * DATE:         November 14,2015
+ *
+ * This example uses the Servo librady from:
+ * http://www.arduino.cc/en/Reference/Servo
+ *
+ * Use the serial console to enter instructions that 
+ * control the servo operations. 
+ *
+ * The user will control the step size (in degrees)
+ * and when to move forward/backwards or reset. 
+ *
+ * The following keys are used for controlling the 
+ * operations of the servo:
+ *   + Increases the step size by 1, but does not 
+ *     change the servo position.
+ *   - Decreases the step size by 1, but does not
+ *     change the servo position.
+ *   F Adds the step size to the current position and 
+ *     moves the motor.
+ *   B Subtracts the step size to the current position  
+ *     and moves the motor.
+ *   R Reset the servo position.
+ *   All other keys are invalid.
+ * 
+ **/ 
+
 #include <Servo.h>
 
 //Servo Control variables
